8a279f8e89fd228bc3ccc40f8a2a94eb2af8faa8
李锦 <lijin@webull.com>
2018-06-13 13:36:50 +0800

    modify

--- dubbo-consummer/src/main/java/com/dubbo/app/App.java
+++ dubbo-consummer/src/main/java/com/dubbo/app/App.java
@@ -1,17 +1,26 @@
 package com.dubbo.app;
 
+import java.io.IOException;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.dubbo.service.DemoService;
 
 public class App {
 	@SuppressWarnings("resource")
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "classpath:/spring/app.xml" });
 		context.start();
 		
-		DemoService svc = (DemoService) context.getBean("demoService");
-		String hello = svc.sayHello("lijin");
-		System.out.println(hello);
+//		MsgService svc = (MsgService) context.getBean("msgService");
+//		UserMsgFrontParam param = new UserMsgFrontParam();
+//    	param.setUserId(100000002);
+//    	UserMsgRedPoint msg = svc.queryUserMsgRedPoint(param);
+//    	System.err.println(msg);
+		
+		DemoService svc = context.getBean(DemoService.class);
+		System.out.println(svc.sayHello("lijin"));
+		
+		System.in.read(); // 保障服务一直运行,利用输入流进行阻塞
 	}
 }
